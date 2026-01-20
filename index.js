047be8aed08de3afc109c3029a8b7ce440b38f1f
DEADLY5067 <deadly5067@gmail.com>
2026-01-20 08:58:58 +0300

    second commit

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const inputEl = document.getElementById("input")
 
 const copyEl = document.querySelector(".fa-copy")
 
-
+const AlertContainerEl = document.querySelector(".alert-container")   
 
 btnEl.addEventListener("click", () => {
     createPassword();
 });
 
 copyEl.addEventListener("click", ()=>{
+    if(inputEl.value){
+        AlertContainerEl.classList.remove("active")
+    setTimeout(()=>{
+        AlertContainerEl.classList.add("active")
+    },2000);
     copuPassword();
+    }
 })
 
 
@@ -27,10 +33,12 @@ function createPassword() {
         password += chars.substring(randomNum, randomNum + 1);      
     }
     inputEl.value = password;
+    navigator.clipboard.writeText(inputEl.value)
+    AlertContainerEl.innerText = password + " copied!";
 }
 
 function copuPassword(){
     inputEl.select();
     inputEl.setSelectionRange(0, 9999);
-    navigator.clipboard.writeText(inputEl.value)
+    
 }
